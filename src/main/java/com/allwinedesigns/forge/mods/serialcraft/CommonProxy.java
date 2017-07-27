b399bb93d1207d7a325205a302c56ceae0d90d4f
John Allwine <jallwine86@gmail.com>
2017-07-26 22:57:24 -0600

    added recipes for serial redstone block and send serial message block

--- src/main/java/com/allwinedesigns/forge/mods/serialcraft/CommonProxy.java
+++ src/main/java/com/allwinedesigns/forge/mods/serialcraft/CommonProxy.java
@@ -6,21 +6,44 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.world.World;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 public class CommonProxy {
-
-	@EventHandler
+    @EventHandler
     public void preinit(FMLPreInitializationEvent event) {
-
-	}
+    }
 	
     @EventHandler
     public void init(FMLInitializationEvent event) {
+        BlockSerialRedstone serialRedstoneBlock = new BlockSerialRedstone();
+        BlockSendSerialMessage sendSerialMessageBlock = new BlockSendSerialMessage();
+
     	GameRegistry.registerTileEntity(TileEntitySerialRedstone.class, "serialRedstone");
-    	GameRegistry.registerBlock(new BlockSerialRedstone(), "serialRedstone");
+    	GameRegistry.registerBlock(serialRedstoneBlock, "serialRedstone");
     	
     	GameRegistry.registerTileEntity(TileEntitySendSerialMessage.class, "sendSerialMessageBlock");
-    	GameRegistry.registerBlock(new BlockSendSerialMessage(), "sendSerialMessageBlock");
+    	GameRegistry.registerBlock(sendSerialMessageBlock, "sendSerialMessageBlock");
+
+        GameRegistry.addRecipe(new ItemStack(serialRedstoneBlock), new Object[]{ 
+          "ABA",
+          "BCB",
+          "ADA",
+          'A', Blocks.redstone_block,
+          'B', Blocks.redstone_ore,
+          'C', Items.clock,
+          'D', Items.sign
+        });
+        GameRegistry.addRecipe(new ItemStack(sendSerialMessageBlock), new Object[]{ 
+          "ABA",
+          "BCB",
+          "ADA",
+          'A', Blocks.iron_block,
+          'B', Blocks.redstone_ore,
+          'C', Items.clock,
+          'D', Items.sign
+        });
 
     }
     
--- src/main/java/com/allwinedesigns/forge/mods/serialcraft/SerialCraft.java
+++ src/main/java/com/allwinedesigns/forge/mods/serialcraft/SerialCraft.java
@@ -16,7 +16,7 @@ public class SerialCraft {
      public static SerialCraft instance;
 	 
     public static final String MODID = "SerialCraft";
-    public static final String VERSION = "1.7.10-0.3-dev";
+    public static final String VERSION = "1.7.10-0.4-dev";
     
 	public static SimpleNetworkWrapper network;
 	
